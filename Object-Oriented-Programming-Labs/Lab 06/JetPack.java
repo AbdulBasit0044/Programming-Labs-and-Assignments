c9c4e19ed205abb96086dc04f87a883e70c2943a
AbdulBasit0044 <AbdulBasit0044@gmail.com>
2017-02-06 15:52:20 +0500

    Create JetPack.java

---
+++ Object-Oriented-Programming-Labs/Lab 06/JetPack.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdul Basit
+ */
+public class JetPack {
+        private double fule;
+    private double liquidjet;
+    private double system;
+    
+    public void targettingsys(){}
+    public void operators(){}
+}
