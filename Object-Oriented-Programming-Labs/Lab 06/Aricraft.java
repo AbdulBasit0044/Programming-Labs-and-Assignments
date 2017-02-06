bb3eb205e008c0bc157d1b6d330c9a4329c7ae34
AbdulBasit0044 <AbdulBasit0044@gmail.com>
2017-02-06 15:51:53 +0500

    Create Aricraft.java

---
+++ Object-Oriented-Programming-Labs/Lab 06/Aricraft.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdul Basit
+ */
+public class Aricraft extends Missile{
+        private double range;
+    
+    public void selfdefence(){}//this can performs self defence
+}
