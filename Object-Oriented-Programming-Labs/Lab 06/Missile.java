b6e12b023729536d6bf976f5af1e3e416cab5beb
AbdulBasit0044 <AbdulBasit0044@gmail.com>
2017-02-07 15:52:56 +0500

    okie

---
+++ Object-Oriented-Programming-Labs/Lab 06/Missile.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdul Basit
+ */
+public class Missile extends Rocket{
+        private double targetsys;
+    private double guidesys;
+    
+    public void launch(){}
+    public void trajectory(){}
+}
