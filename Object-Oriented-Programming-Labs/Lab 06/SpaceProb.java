5cb12f62384652f984f1490729fecc6cc663df9f
AbdulBasit0044 <AbdulBasit0044@gmail.com>
2017-02-07 15:53:15 +0500

    Create SpaceProb.java

---
+++ Object-Oriented-Programming-Labs/Lab 06/SpaceProb.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdul Basit
+ */
+public class SpaceProb extends Rocket {
+        private double sojourne;
+    private double spacecraft;
+    
+    public void accuracy(){}
+    public void destructing(){}
+}
